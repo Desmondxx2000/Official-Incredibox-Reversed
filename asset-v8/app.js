804e17c24c8785cca9352b06700c8bf7aae4edaf
Desmondxx2000 <157266806+Desmondxx2000@users.noreply.github.com>
2024-02-29 16:50:54 -0500

    Add files via upload

--- asset-v8/app.js
+++ asset-v8/app.js
@@ -1,39 +1,39 @@
 /* 0.5.4 2021-04-23 21:06:13 */
 var app = new function() {
     this.name = "Dystopia", this.version = "8", this.date = "2020", this.folder = "asset-v8/", this.looptime = 6857, this.bpm = 140, this.totalframe = 328, this.nbpolo = 7, this.nbloopbonus = 6, this.bonusloopA = !0, this.bonusendloopA = !1, this.recmaxloop = 27, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#0a1d1e", this.col0 = "#779295", this.col1 = "#5f7e7b", this.col2 = "#476761", this.col3 = "#304d47", this.col4 = "#1b312f", this.animearray = [{
-        name: "01_atlanta",
+        name: "1_atlanta",
         color: "5f7e7b",
         uniqsnd: !1
     }, {
-        name: "02_tuctom",
+        name: "2_tuctom",
         color: "5f7e7b",
         uniqsnd: !1
     }, {
-        name: "03_foubreak",
+        name: "3_foubreak",
         color: "5f7e7b",
         uniqsnd: !1
     }, {
-        name: "04_koukaki",
+        name: "4_koukaki",
         color: "5f7e7b",
         uniqsnd: !0
     }, {
-        name: "05_koungou",
+        name: "5_koungou",
         color: "5f7e7b",
         uniqsnd: !0
     }, {
-        name: "06_bass",
+        name: "6_bass",
         color: "5f7e7b",
         uniqsnd: !1
     }, {
-        name: "07_monk",
+        name: "7_monk",
         color: "5f7e7b",
         uniqsnd: !1
     }, {
-        name: "08_sonar",
+        name: "8_sonar",
         color: "5f7e7b",
         uniqsnd: !0
     }, {
-        name: "09_souffle",
+        name: "9_souffle",
         color: "5f7e7b",
         uniqsnd: !1
     }, {
